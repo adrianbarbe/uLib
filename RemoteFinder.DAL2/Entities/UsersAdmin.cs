a8cf7ed49b00f89274e3fde83601849f4bd2c969
Adrian Barbe <adryanbarbe@gmail.com>
2022-11-02 18:19:27 +0200

    Entities

---
+++ RemoteFinder.DAL2/Entities/UsersAdmin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoteFinder.DAL2.Entities
+{
+    public partial class UsersAdmin
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int? LoginsCount { get; set; }
+        public DateTime? LastLogin { get; set; }
+    }
+}
---
+++ RemoteFinder.DAL2/Entities/UsersSocial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoteFinder.DAL2.Entities
+{
+    public partial class UsersSocial
+    {
+        public UsersSocial()
+        {
+            Files = new HashSet<File>();
+            Payments = new HashSet<Payment>();
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserPicture { get; set; }
+        public string ProviderUserId { get; set; }
+        public int OauthProvider { get; set; }
+
+        public virtual ICollection<File> Files { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
