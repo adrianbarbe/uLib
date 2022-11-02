7245d03d161c2cd810f2501dd51592fbb91de876
Adrian Barbe <adryanbarbe@gmail.com>
2022-11-02 23:23:03 +0200

    Added simple crud in mvc

--- RemoteFinder.BLL/Services/FileService/IFileService.cs
+++ RemoteFinder.BLL/Services/FileService/IFileService.cs
@@ -5,4 +5,12 @@ namespace RemoteFinder.BLL.Services.FileService;
 public interface IFileService
 { 
     List<File> GetAll();
+
+    File Get(int id);
+
+    File Create(File file);
+
+    File Update(int id, File file);
+
+    void Delete(int id);
 }
