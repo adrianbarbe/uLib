377635d5708c91f0b662630eec2ba2287a0a354d
Adrian Barbe <adryanbarbe@gmail.com>
2023-01-02 22:43:44 +0200

    Config fixes

--- RemoteFinder.UI/src/components/auth/SignUp.vue
+++ RemoteFinder.UI/src/components/auth/SignUp.vue
@@ -33,7 +33,7 @@ export default {
                 scope: 'email openid profile',
                 access_type: 'offline',
                 response_type: 'code',
-                redirect_uri: `http://localhost:8080/auth/redirect-sign-up`,
+                redirect_uri: `${this.$config.apiEndpoint}/auth/redirect-sign-up`,
                 client_id: clientId,
             };
 
